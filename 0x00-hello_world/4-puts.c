8b5061bd0ed92c54992e222b13a74e8746d0c110
MrPotato01 <emailno299@gmail.com>
2023-04-02 22:16:05 -0700

    ok

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+<<<<<<< HEAD
+int main()
+{	
+	char words[] = "Programming is like building a multilingual puzzle";
+	puts(words);
+        return 0;
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+>>>>>>> b9ce1b4e923339ceaf2030f3f05f1a0426d2cb78
+}
